e2a3a5fc9c7370be682b64e49b49f1a62402f38f
michealroberts <michael@observerly.com>
2024-10-27 13:22:31 +0000

    feat: add (g *GAIAServiceClient) Build() to catalog module in @observerly/skysolve
    feat: add (g *GAIAServiceClient) Build() to catalog module in @observerly/skysolve

--- pkg/catalog/gaia.go
+++ pkg/catalog/gaia.go
@@ -9,6 +9,12 @@
 package catalog
 
 /*****************************************************************************************************************/
+
+import (
+	"bytes"
+	"text/template"
+)
+
 /*****************************************************************************************************************/
 
 type GAIAQuery struct {
@@ -32,6 +38,58 @@ func NewGAIAServiceClient() *GAIAServiceClient {
 		URI:   "https://gea.esac.esa.int/tap-server/tap/sync",
 		Query: GAIAQuery{},
 	}
+}
+
+/*****************************************************************************************************************/
+
+const record = `source_id, designation, ra, dec, pmra, pmdec, parallax, phot_g_mean_flux, phot_g_mean_mag`
+
+/*****************************************************************************************************************/
+
+func (g *GAIAServiceClient) Build() (string, error) {
+	// Define the ADQL query template for the GAIA TAP service:
+	// @see https://gea.esac.esa.int/archive/documentation/GDR3/Gaia_archive/chap_datamodel/
+	// N.B. (use only gold standard data, e.g., photometry processing mode (byte) i.e., phot_proc_mode = '0'):
+	const queryTemplate = `
+		SELECT {{.Record}}
+		FROM gaiadr3.gaia_source
+		WHERE CONTAINS(
+			POINT('ICRS', ra, dec),
+			CIRCLE('ICRS', {{.RA}}, {{.Dec}}, {{.Radius}})
+		) = 1 AND phot_g_mean_mag < {{.Limit}} AND phot_proc_mode = '0'
+	`
+
+	// Parse the ADQL query template:
+	tmpl, err := template.New("adql").Parse(queryTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	// Set the template data from the query parameters:
+	// Data to populate the template
+	data := struct {
+		Record string
+		RA     float64
+		Dec    float64
+		Radius float64
+		Limit  float64
+	}{
+		Record: record,
+		RA:     g.Query.RA,
+		Dec:    g.Query.Dec,
+		Radius: g.Query.Radius,
+		Limit:  g.Query.Limit,
+	}
+
+	// Execute the template with the data:
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, data)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the ADQL query string:
+	return buf.String(), nil
 }
 
 /*****************************************************************************************************************/
