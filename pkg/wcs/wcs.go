0e28c0f3b73f91ed38a4e7a41241fb65074d6ae1
michealroberts <michael@observerly.com>
2024-11-06 11:46:46 +0000

    feat: add SIP transform.SIP2DParameters key to WCS struct in @observerly/skysolve
    feat: add SIP transform.SIP2DParameters key to WCS struct in @observerly/skysolve

--- pkg/wcs/wcs.go
+++ pkg/wcs/wcs.go
@@ -66,23 +66,24 @@ type WCSParams struct {
 /*****************************************************************************************************************/
 
 type WCS struct {
-	WCAXES int     `hdu:"WCAXES" default:"2"`        // Number of world coordinate axes
-	CRPIX1 float64 `hdu:"CRPIX1"`                    // Reference pixel X
-	CRPIX2 float64 `hdu:"CRPIX2"`                    // Reference pixel Y
-	CRVAL1 float64 `hdu:"CRVAL1" default:"0.0"`      // Reference RA (example default, often specific to image)
-	CRVAL2 float64 `hdu:"CRVAL2" default:"0.0"`      // Reference Dec (example default, often specific to image)
-	CTYPE1 string  `hdu:"CTYPE1" default:"RA---TAN"` // Coordinate type for axis 1, typically RA with TAN projection
-	CTYPE2 string  `hdu:"CTYPE2" default:"DEC--TAN"` // Coordinate type for axis 2, typically DEC with TAN projection
-	CDELT1 float64 `hdu:"CDELT1"`                    // Coordinate increment for axis 1 (no default)
-	CDELT2 float64 `hdu:"CDELT2"`                    // Coordinate increment for axis 2 (no default)
-	CUNIT1 string  `hdu:"CUNIT1" default:"deg"`      // Coordinate unit for axis 1, defaulted to degrees
-	CUNIT2 string  `hdu:"CUNIT2" default:"deg"`      // Coordinate unit for axis 2, defaulted to degrees
-	CD1_1  float64 `hdu:"CD1_1"`                     // Affine transform parameter A (no default)
-	CD1_2  float64 `hdu:"CD1_2"`                     // Affine transform parameter B (no default)
-	CD2_1  float64 `hdu:"CD2_1"`                     // Affine transform parameter C (no default)
-	CD2_2  float64 `hdu:"CD2_2"`                     // Affine transform parameter D (no default)
-	E      float64 `hdu:"E"`                         // Affine translation parameter e (optional, no default)
-	F      float64 `hdu:"F"`                         // Affine translation parameter f (optional, no default)
+	WCAXES int                       `hdu:"WCAXES" default:"2"`        // Number of world coordinate axes
+	CRPIX1 float64                   `hdu:"CRPIX1"`                    // Reference pixel X
+	CRPIX2 float64                   `hdu:"CRPIX2"`                    // Reference pixel Y
+	CRVAL1 float64                   `hdu:"CRVAL1" default:"0.0"`      // Reference RA (example default, often specific to image)
+	CRVAL2 float64                   `hdu:"CRVAL2" default:"0.0"`      // Reference Dec (example default, often specific to image)
+	CTYPE1 string                    `hdu:"CTYPE1" default:"RA---TAN"` // Coordinate type for axis 1, typically RA with TAN projection
+	CTYPE2 string                    `hdu:"CTYPE2" default:"DEC--TAN"` // Coordinate type for axis 2, typically DEC with TAN projection
+	CDELT1 float64                   `hdu:"CDELT1"`                    // Coordinate increment for axis 1 (no default)
+	CDELT2 float64                   `hdu:"CDELT2"`                    // Coordinate increment for axis 2 (no default)
+	CUNIT1 string                    `hdu:"CUNIT1" default:"deg"`      // Coordinate unit for axis 1, defaulted to degrees
+	CUNIT2 string                    `hdu:"CUNIT2" default:"deg"`      // Coordinate unit for axis 2, defaulted to degrees
+	CD1_1  float64                   `hdu:"CD1_1"`                     // Affine transform parameter A (no default)
+	CD1_2  float64                   `hdu:"CD1_2"`                     // Affine transform parameter B (no default)
+	CD2_1  float64                   `hdu:"CD2_1"`                     // Affine transform parameter C (no default)
+	CD2_2  float64                   `hdu:"CD2_2"`                     // Affine transform parameter D (no default)
+	E      float64                   `hdu:"E"`                         // Affine translation parameter e (optional, no default)
+	F      float64                   `hdu:"F"`                         // Affine translation parameter f (optional, no default)
+	SIP    transform.SIP2DParameters // SIP transformation (distortion) coefficients
 }
 
 /*****************************************************************************************************************/
@@ -108,6 +109,7 @@ func NewWorldCoordinateSystem(xc float64, yc float64, params WCSParams) WCS {
 		CD2_2:  params.AffineParams.D,
 		E:      params.AffineParams.E,
 		F:      params.AffineParams.F,
+		SIP:    params.SIPParams,
 	}
 
 	// Calculate the reference equatorial coordinate:
