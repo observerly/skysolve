ca6cf15e66249574ff76636e99279505f03dcd68
michealroberts <michael@observerly.com>
2024-11-20 17:32:55 +0000

    feat: add NewTapClient(serviceURL url.URL) to adql module in @observerly/skysolve
    feat: add NewTapClient(serviceURL url.URL) to adql module in @observerly/skysolve

--- pkg/adql/adql.go
+++ pkg/adql/adql.go
@@ -32,3 +32,19 @@ type TapClient struct {
 }
 
 /*****************************************************************************************************************/
+
+// NewTapClient initializes a new generic TAP ADQL client with optional configurations.
+func NewTapClient(serviceURL url.URL, timeout time.Duration, headers map[string]string) *TapClient {
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	return &TapClient{
+		URI:     serviceURL.String(),
+		Client:  client,
+		Timeout: timeout,
+		Headers: headers,
+	}
+}
+
+/*****************************************************************************************************************/
