eaa65e74a5a5dcd9f70eba779d52f7f321759c34
michealroberts <michael@observerly.com>
2024-11-25 14:17:29 +0000

    feat: add SIMBADServiceClient type to catalog module in @observerly/skysolve
    feat: add SIMBADServiceClient type to catalog module in @observerly/skysolve

--- pkg/catalog/simbad.go
+++ pkg/catalog/simbad.go
@@ -10,12 +10,25 @@ package catalog
 
 /*****************************************************************************************************************/
 
+import (
+	"github.com/observerly/skysolve/pkg/adql"
+)
+
+/*****************************************************************************************************************/
+
 type SIMBADQuery struct {
 	RA        float64 // right ascension (in degrees)
 	Dec       float64 // right ascension (in degrees)
 	Radius    float64 // search radius (in degrees)
 	Limit     int     // maximum number of records to return
 	Threshold float64 // limiting magnitude
+}
+
+/*****************************************************************************************************************/
+
+type SIMBADServiceClient struct {
+	*adql.TapClient
+	Query SIMBADQuery
 }
 
 /*****************************************************************************************************************/
