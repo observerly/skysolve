06c3cd70f3fdf2555d47c1d29f38af3e82945a56
michealroberts <michael@observerly.com>
2024-12-05 20:32:19 +0000

    feat: add NewHealPIX() *HealPIX to healpix module in @observerly/skysolve
    feat: add NewHealPIX() *HealPIX to healpix module in @observerly/skysolve

--- pkg/healpix/healpix.go
+++ pkg/healpix/healpix.go
@@ -17,3 +17,15 @@ type HealPIX struct {
 }
 
 /*****************************************************************************************************************/
+
+// HEALPix, i.e., the "Hierarchical Equal Area isoLatitude Pixelization", is a versatile structure for the
+// pixelization of coordinates on the sphere.
+func NewHealPIX() *HealPIX {
+	return &HealPIX{
+		Longitude:             180.0,
+		Latitude:              0.0,
+		PolarLatitudeBoundary: 2.0 / 3.0, // in radians (approximately 38.1972 degrees)
+	}
+}
+
+/*****************************************************************************************************************/
