f6bd3a4f0d974f7e46c82a5e83e77fadb2663b96
michealroberts <michael@observerly.com>
2024-12-15 15:27:30 +0000

    feat: add NewPlateSolver to solve module in @observerly/skysolve
    feat: add NewPlateSolver to solve module in @observerly/skysolve

--- pkg/solve/solve.go
+++ pkg/solve/solve.go
@@ -11,7 +11,12 @@ package solve
 /*****************************************************************************************************************/
 
 import (
+	"math"
+	"sort"
+	"sync"
+
 	"github.com/observerly/iris/pkg/photometry"
+	stats "github.com/observerly/iris/pkg/statistics"
 
 	"github.com/observerly/skysolve/pkg/catalog"
 )
@@ -42,6 +47,61 @@ type Params struct {
 	ExtractionThreshold float64
 	Radius              float64
 	Sigma               float64
+}
+
+/*****************************************************************************************************************/
+
+// NewPlateSolver initializes a new PlateSolver with the given FITS image and parameters.
+func NewPlateSolver(
+	params Params,
+) (*PlateSolver, error) {
+	radius := params.Radius
+	sigma := params.Sigma
+	stars := []photometry.Star{}
+	sources := []catalog.Source{}
+
+	// Calculate the width of the image in pixels:
+	xs := params.Width
+
+	// Calculate the height of the image in pixels:
+	ys := params.Height
+
+	// Setup a wait group for the stars extractor:
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Extract bright pixels (stars) from the image:
+	go func() {
+		defer wg.Done()
+
+		// Extract the image from the FITS file:
+		sexp := photometry.NewStarsExtractor(params.Data, xs, ys, float32(radius), params.ADU)
+
+		// Extract the bright pixels from the image:
+		starsExtracted := sexp.FindStars(stats.NewStats(params.Data, params.ADU, xs), float32(sigma), 2.2)
+
+		// Sort the stars by intensity, in descending order:
+		sort.Slice(starsExtracted, func(i, j int) bool {
+			return starsExtracted[i].Intensity > starsExtracted[j].Intensity
+		})
+
+		// Get a minimum of X stars from our list of stars, e.g., the brightest X stars:
+		k := math.Min(float64(len(starsExtracted)), params.ExtractionThreshold)
+
+		stars = starsExtracted[:int(k)]
+	}()
+
+	// Wait for the stars extractor to finish
+	wg.Wait()
+
+	// Return a new PlateSolver object with the catalog, stars, sources, RA, Dec, and pixel scale:
+	return &PlateSolver{
+		Stars:   stars,
+		Sources: sources,
+		Data:    params.Data,
+		Width:   xs,
+		Height:  ys,
+	}, nil
 }
 
 /*****************************************************************************************************************/
